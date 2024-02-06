50b670439fbc238a87b86f047aa903b60bda8188
shusaku009 <shusaku009@gmail.com>
2024-02-06 09:47:15 +0900

    Add: コンポーネントのストリーミング

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import styles from '@/app/ui/home.module.css'
+import styles from '@/app/ui/home.module.css';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
@@ -14,7 +14,9 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape} />
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`} />
+          <p
+            className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          />
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
@@ -47,7 +49,6 @@ export default function Page() {
             className="block md:hidden"
             alt="Screenshots of the dashboard project showing desktop version"
           />
-
         </div>
       </div>
     </main>
